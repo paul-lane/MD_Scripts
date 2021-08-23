5481cec9e871e424a590639210bce046aa8ff826
paul-lane <64542047+paul-lane@users.noreply.github.com>
2021-08-23 11:20:37 +0100

    Uploaded populatebox code

---
+++ checknResidue.py
@@ -0,0 +1,24 @@
+import sys
+import MDAnalysis as mda
+from MDAnalysis.tests.datafiles import PSF, DCD, GRO, XTC
+
+filename =  str(sys.argv[1])
+
+#print(filename)
+
+
+##############################################################
+# Set up the .gro file Universe
+##############################################################
+
+# Create Universe from .gro file
+grofile = mda.Universe(filename)
+
+# Get number of residues in Universe
+nresidue = len(grofile.residues)
+#print(nresidue)
+
+# write number of residues to file
+f=open("residues.txt", "w")
+f.write(str(nresidue))
+f.close()
